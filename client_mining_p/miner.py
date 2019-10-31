22acabc43b3cc3e6fb3b46d73cc986bf1c7f88dd
Chase Wex <clapingmanly@hotmail.com>
2019-10-31 14:31:37 -0400

    added basic block chaining

--- client_mining_p/miner.py
+++ client_mining_p/miner.py
@@ -4,7 +4,6 @@ import requests
 import sys
 import json
 
-
 def proof_of_work(block):
     """
     Simple Proof of Work Algorithm
@@ -13,10 +12,29 @@ def proof_of_work(block):
     in an effort to find a number that is a valid proof
     :return: A valid proof for the provided block
     """
-    pass
+    block_string = json.dumps(block, sort_keys=True).encode()
+    proof = 0
+    while not valid_proof(block_string, proof):
+        proof += 1
+    return proof
 
 
 def valid_proof(block_string, proof):
+    """
+    Validates the Proof:  Does hash(block_string, proof) contain 3
+    leading zeroes?  Return true if the proof is valid
+    :param block_string: <string> The stringified block to use to
+    check in combination with `proof`
+    :param proof: <int?> The value that when combined with the
+    stringified previous block results in a hash that has the
+    correct number of leading zeroes.
+    :return: True if the resulting hash is a valid proof, False otherwise
+    """
+    guess = f'{block_string}{proof}'.encode()
+    guess_hash = hashlib.sha256(guess).hexdigest()
+    # return True or False
+    return guess_hash[:6] == "000000"
+# Instantiate our Node
     """
     Validates the Proof:  Does hash(block_string, proof) contain 6
     leading zeroes?  Return true if the proof is valid
@@ -27,7 +45,6 @@ def valid_proof(block_string, proof):
     correct number of leading zeroes.
     :return: True if the resulting hash is a valid proof, False otherwise
     """
-    pass
 
 
 if __name__ == '__main__':
@@ -38,11 +55,12 @@ if __name__ == '__main__':
         node = "http://localhost:5000"
 
     # Load ID
-    f = open("my_id.txt", "r")
+    """    f = open("my_id.txt", "r")
     id = f.read()
     print("ID is", id)
-    f.close()
-
+    f.close() """
+    id = 123123123
+    print(node);
     # Run forever until interrupted
     while True:
         r = requests.get(url=node + "/last_block")
@@ -57,13 +75,22 @@ if __name__ == '__main__':
 
         # TODO: Get the block from `data` and use it to look for a new proof
         # new_proof = ???
-
+        new_proof = proof_of_work(data["lastblock"]);
         # When found, POST it to the server {"proof": new_proof, "id": id}
-        post_data = {"proof": new_proof, "id": id}
-
+        post_data = {"proof": str(new_proof), "id": str(id)}
         r = requests.post(url=node + "/mine", json=post_data)
-        data = r.json()
-
+        try:
+            data = r.json()
+        except:
+            print("Error:  Non-json response")
+            print("Response returned:")
+            print(r)
+            break
+        if(r.status_code == 200):
+            print("mined a coin?");
+        else:
+            print(data["message"], new_proof);
+        
         # TODO: If the server responds with a 'message' 'New Block Forged'
         # add 1 to the number of coins mined and print it.  Otherwise,
         # print the message from the server.
